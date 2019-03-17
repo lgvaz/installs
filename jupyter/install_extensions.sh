cedf50568eac0fe5c91d3994318896dd88db88a8
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 18:16:17 +0530

    Update install_extensions.sh

--- jupyter/install_extensions.sh
+++ jupyter/install_extensions.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install jupyter notebook and configure to accessible via internet
-conda install -c anaconda jupyter
+conda install -y -c anaconda jupyter
 jupyter notebook --generate-config
 echo "c.NotebookApp.ip = '0.0.0.0'" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
