0ef874a7de78bbcb4648b55922c91235b6468a44
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:45:47 +0530

    backup copy bashrc

--- linux/install_python.sh
+++ linux/install_python.sh
@@ -3,6 +3,7 @@ mkdir -p Downloads && cd Downloads
 !wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
 bash Anaconda3-2018.12-Linux-x86_64.sh -b
 
+cp $HOME/.bashrc $HOME/.bashrc_backup
 echo "Add Anaconda bindings to PATH" >> $HOME/.bashrc
 echo "export PATH=\"$HOME/anaconda3/bin:$PATH\"" >> $HOME/.bashrc
 source $HOME/.bashrc
