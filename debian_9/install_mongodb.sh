c1ae4080f6aa463c07eab9543a41b5fb1f42c3a7
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:59:05 +0530

    Update install_mongodb.sh

--- debian_9/install_mongodb.sh
+++ debian_9/install_mongodb.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+
+# Dependencies
+sudo apt update && sudo apt install dirmngr
 # Add keys
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 # Create the list file
