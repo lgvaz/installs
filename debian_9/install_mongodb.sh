2a1a85a243c37e0a57b5fb89e50a006b1718eef0
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 14:12:59 +0530

    Create install_mongodb.sh

---
+++ debian_9/install_mongodb.sh
@@ -0,0 +1,7 @@
+# Add keys
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+# Create the list file
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
