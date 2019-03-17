2443149c6c15db6c4cdab75b297f55254a55ac32
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 18:19:47 +0530

    Update install_extensions.sh

--- jupyter/install_extensions.sh
+++ jupyter/install_extensions.sh
@@ -7,8 +7,6 @@ jupyter notebook --generate-config
 echo "c.NotebookApp.ip = '0.0.0.0'" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
 
-/home/lgvaz/.jupyter/jupyter_notebook_config.py
-
 # Themes and extensions
 pip install jupyterthemes
 pip install jupyter_contrib_nbextensions
