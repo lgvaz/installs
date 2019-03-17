00adc4d532e8ac28c8bd61cd517ad93a36d2ba3a
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 18:15:12 +0530

    Update install_extensions.sh

--- jupyter/install_extensions.sh
+++ jupyter/install_extensions.sh
@@ -1,6 +1,15 @@
 #!bin/bash
 set -e
 
+# Install jupyter notebook and configure to accessible via internet
+conda install -c anaconda jupyter
+jupyter notebook --generate-config
+echo "c.NotebookApp.ip = '0.0.0.0'" >> $HOME/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
+
+/home/lgvaz/.jupyter/jupyter_notebook_config.py
+
+# Themes and extensions
 pip install jupyterthemes
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
