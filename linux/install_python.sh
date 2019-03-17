c8e9a1322a305d266fce59ef96de28854283c93c
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:01:35 +0530

    Create install_python.sh

---
+++ linux/install_python.sh
@@ -0,0 +1,8 @@
+mkdir -p Downloads && cd Downloads
+
+!wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
+bash Anaconda3-2018.12-Linux-x86_64.sh -b
+
+echo "Add Anaconda bindings to PATH" >> $HOME/.bashrc
+echo "export PATH=\"$HOME/anaconda3/bin:$PATH\"" >> $HOME/.bashrc
+source $HOME/.bashrc
