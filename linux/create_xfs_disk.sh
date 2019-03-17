922d134f9d2f37a9e3a040528c4e11044ee95204
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:25:54 +0530

    Create create_xfs_disk.sh

---
+++ linux/create_xfs_disk.sh
@@ -0,0 +1,12 @@
+DISK=$1
+MOUNT_PATH=$2
+
+echo "Formating disk: $DISK"
+echo "Disk will be mounted on: $MOUNT_PATH"
+
+sudo apt update && sudo apt install xfsprogs
+sudo mkdir -p $MOUNT_PATH
+sudo mkfs.xfs -f $DISK
+sudo mount -t xfs $DISK $MOUNT_PATH
+
+sudo echo "$DISK  /storage xfs  defaults  0  0" >> /etc/fstab
