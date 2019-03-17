91165cfe85cc15b51c568c934c2b7bd12479dc82
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:37:11 +0530

    fdisk

--- linux/create_xfs_disk.sh
+++ linux/create_xfs_disk.sh
@@ -2,13 +2,15 @@
 set -e
 
 DISK=$1
-DISK+=1
 MOUNT_PATH=$2
 
 echo "Formating disk: $DISK"
 echo "Disk will be mounted on: $MOUNT_PATH"
 
 sudo apt update && sudo apt install -y xfsprogs
+sudo fdisk $DISK
+DISK+=1
+# TODO: Assumes disk is created in partition ONE
 sudo mkdir -p $MOUNT_PATH
 sudo mkfs.xfs -f $DISK
 sudo mount -t xfs $DISK $MOUNT_PATH
