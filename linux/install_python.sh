524da42d810a89f26cd79da189b1936570607510
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-17 15:50:01 +0530

    force conda installation

--- linux/install_python.sh
+++ linux/install_python.sh
@@ -6,7 +6,7 @@ mkdir -p $DIR && cd $DIR
 
 sudo apt update && sudo apt install -y bzip2
 wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
-bash Anaconda3-2018.12-Linux-x86_64.sh -b
+bash Anaconda3-2018.12-Linux-x86_64.sh -b -f
 
 cp $HOME/.bashrc $HOME/.bashrc_backup
 echo "Add Anaconda bindings to PATH" >> $HOME/.bashrc
