511d31213945141bdd7264b79dc3461f65b48ad1
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-24 16:42:10 +0530

    Update install_fasttext.sh

--- linux/install_fasttext.sh
+++ linux/install_fasttext.sh
@@ -11,6 +11,7 @@ cd fastText && make
 # Clone model
 mkdir -p $HOME/models/fastText && cd $HOME/models/fastText
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
+gunzip -k cc.en.300.bin.gz
 
 # Create shortcut
 echo "##### Add fasttext shortcut ####" >> $HOME/.bashrc
