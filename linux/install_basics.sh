a51ff473c3a6c49e709649401e1c234d96b78ca3
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-03-31 12:50:53 +0530

    Update install_basics.sh

--- linux/install_basics.sh
+++ linux/install_basics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y git tmux htop g++ clang make xfsprogs
+sudo apt install -y git tmux htop g++ clang make xfsprogs unzip
 
 git config --global user.name "Lucas Vazquez"
 git config --global user.email lgvaz@lishash.com
