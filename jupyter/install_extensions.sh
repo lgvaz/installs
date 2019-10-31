21ccffd607fac862bd7f102c55e6715217e8b110
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-31 18:44:01 +0000

    only install extensions

--- jupyter/install_extensions.sh
+++ jupyter/install_extensions.sh
@@ -2,16 +2,12 @@
 set -e
 
 # Install jupyter notebook and configure to accessible via internet
-conda install -y -c anaconda jupyter
-jupyter notebook --generate-config
-echo "c.NotebookApp.ip = '0.0.0.0'" >> $HOME/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
+# conda install -y -c anaconda jupyter
+# jupyter notebook --generate-config
+# echo "c.NotebookApp.ip = '0.0.0.0'" >> $HOME/.jupyter/jupyter_notebook_config.py
+# echo "c.NotebookApp.port = 8888" >> $HOME/.jupyter/jupyter_notebook_config.py
 
 # Themes and extensions
+conda install -c conda-forge jupyter_contrib_nbextensions
 pip install jupyterthemes
-pip install jupyter_contrib_nbextensions
-jupyter contrib nbextension install --user
-pip install jupyter_nbextensions_configurator
-jupyter nbextensions_configurator enable --user
-
 jt -t onedork -cellw 1280
