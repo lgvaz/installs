6cc2a40a9618ef37ace597e966c64718a035b739
Lucas Vazquez <lucasgouvaz@gmail.com>
2019-11-08 23:32:06 -0300

    Update install_python.sh

--- linux/install_python.sh
+++ linux/install_python.sh
@@ -2,15 +2,15 @@
 set -e
 
 DIR=$HOME/Downloads
+CONDA=Anaconda3-2019.10-Linux-x86_64.sh
 mkdir -p $DIR && cd $DIR
 
-apt-get update && apt-get install -y bzip2
-wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-bash Anaconda3-2018.12-Linux-x86_64.sh -b -f
+apt-get install -y bzip2
+wget https://repo.anaconda.com/archive/$CONDA
+bash $CONDA -b -f
 
-cp $HOME/.bashrc $HOME/.bashrc_backup
 echo "##### Add Anaconda bindings to PATH #####" >> $HOME/.bashrc
-echo ". $HOME/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+echo ". $HOME/anaconda3/etc/profile.d/conda.sh" >> $HOME/.bashrc
 source $HOME/.bashrc
 
 conda update -n base -c defaults conda
